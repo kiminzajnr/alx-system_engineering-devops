2f2a735972a0276f04bafe2538d9db9ba10a46b5
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-15 13:24:40 +0300

    client configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# enables connection to server without a password
+
+file_line {
+  'passAuth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+file_line {
+  'keyLocation':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton'.
+}
