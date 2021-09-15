215e9d7dd88af6785a51642b5d29f45030ffd396
Erick Kiminza <kiminzaeric@gmail.com>
2021-09-15 16:56:57 +0300

    fix output errors

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,10 +16,16 @@ if __name__ == "__main__":
 
     users = r_for_users.json()
     todos = r_for_todos.json()
-    print(len(users))
+    todo_all_employees = {}
+
+    for user in users:
+        todos_list = []
+        for todo in todos:
+            if todo.get("userId") == user.get("id"):
+                my_dict = {"username": user.get("username"),
+                           "task": todo.get("title"),
+                           "completed": todo.get("completed")}
+                todos_list.append(my_dict)
+        todo_all_employees[user.get("id")] = todos_list
     with open(FILENAME, 'w+') as f:
-        for user in users:
-            json.dump({user.get("id"): [{
-                "username": user.get("username"),
-                "task": todo.get("title"),
-                "completed": todo.get("completed")} for todo in todos]}, f)
+        json.dump(todo_all_employees, f)
