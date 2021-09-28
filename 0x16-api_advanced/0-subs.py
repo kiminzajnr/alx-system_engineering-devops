ab44cecd56e9c2c7eb08b961eca13c76ceed1d7b
Erick Kiminza <kiminzaeric@gmail.com>
2021-09-28 18:35:41 +0300

    fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,9 @@ queries the Reddit API and returns the number of subscribers(not active
 users, total subscribers) for a given subreddit. if an invalid subreddit
 is given, the function should return 0.
 """
-
-
 import requests
+
+
 def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     headers = {
