5286b5989ecf0685b94906bf9559cbfbbbdd3d69
Erick Kiminza <kiminzaeric@gmail.com>
2021-10-07 10:20:14 +0300

    fix typo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix our stack so that there is not failed requests
 
 exec { 'change nginx limit':
-    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart'
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
     provider => shell,
 }
