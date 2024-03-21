78626c39d5de350dc62714f817e3af5b54831570
kiminzajnr <kiminzaeric@gmail.com>
2024-03-21 21:20:58 +0300

    check if user passed the id

--- 0x15-api/gather.py
+++ 0x15-api/gather.py
@@ -1,24 +1,31 @@
 import requests, sys
 
+
 todos_data = requests.get('https://jsonplaceholder.typicode.com/todos')
 users_data = requests.get('https://jsonplaceholder.typicode.com/users')
+
+if len(sys.argv) < 2:
+    print("Usage: python3 script_name.py <USER_ID>")
+    sys.exit()
+else:
+    USER_ID = int(sys.argv[1])
 
 TITLES = ""
 EMPLOYEE_NAME = ""
 NUMBER_OF_DONE_TASKS = 0
 NUMBER_OF_UNDONE_TASKS = 0
 
-emp_id = int(sys.argv[1])
+
 for dct in users_data.json():
-    if dct.get('id') == emp_id:
+    if dct.get('id') == USER_ID:
         EMPLOYEE_NAME = dct.get('name')
 
 for item in todos_data.json():
-    if item.get('userId') == emp_id and item.get('completed') == True:
+    if item.get('userId') == USER_ID and item.get('completed') == True:
         NUMBER_OF_DONE_TASKS += 1
         TITLES += "     " + item.get('title') + "\n"
     else:
-        if item.get('userId') == emp_id and item.get('completed') == False:
+        if item.get('userId') == USER_ID and item.get('completed') == False:
            NUMBER_OF_UNDONE_TASKS += 1 
 
 TOTAL_NUMBER_OF_TASKS = NUMBER_OF_DONE_TASKS + NUMBER_OF_UNDONE_TASKS
