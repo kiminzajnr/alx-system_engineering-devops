671a4832a8e9fdf6bad4150795addc4d58c60244
kiminzajnr <kiminzaeric@gmail.com>
2024-03-23 12:45:11 +0300

    update

--- 0x15-api/export_to_CSV.py
+++ 0x15-api/export_to_CSV.py
@@ -19,5 +19,5 @@ with open('output.csv', 'w') as csvfile:
     csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
     # csvwriter.writerows(["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"])
     for item in todos_data.json():
-        if item.get('userId'):
-            csvwriter.writerow([USER_ID, USERNAME, item.get('completed'), item.get('title')])
+        # if item.get('userId'):
+        csvwriter.writerow([USER_ID, USERNAME, item.get('completed'), item.get('title')])
